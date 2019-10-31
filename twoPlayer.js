701c7aec1363c998694fd06c5eae53ed69a47e69
Zack Amin <ishaqhamin@gmail.com>
2019-10-31 10:32:41 +0000

    twoPlayers

--- twoPlayer.js
+++ twoPlayer.js
@@ -1,7 +1,9 @@
 var myGamePiece;
+var tieFighter;
 
 function startGame() {
-    myGamePiece = new component(80, 80, "mfalcon.png", 500, 420, "image");
+    myGamePiece = new component(80, 80, "mfalcon.png", 450, 420, "image");
+    tieFighter = new component (70, 70, "tiefighter.png", 450, 20, "image");
     myGameArea.start();
 }
 
@@ -30,7 +32,7 @@ var myGameArea = {
 }
 
 // This function relates to the ship size, image and height.
-function component(width, height, color, x, y, type) {
+function component (width, height, color, x, y, type) {
     this.type = type;
     if (type == "image") {
         this.image = new Image();
@@ -60,6 +62,7 @@ function component(width, height, color, x, y, type) {
     }
 }
 
+
 // This function sets the boundaries the ship cannot pass. 1st one is the left boundary, 
 // 2nd one is the right boundary, 3rd one is the bottom boundary.
 var rightBorder = canvas.width - myGamePiece.width;
@@ -75,6 +78,19 @@ function playerBounds() {
         myGamePiece.y = 430;
     }
 }
+var rightBorder = canvas.width - tieFighter.width;
+
+function tieBounds() {
+    if (tieFighter.x < 0) {
+        tieFighter.x = 0;
+    }
+    if (tieFighter.x > 930) {
+        tieFighter.x = 930;
+    }
+    if (tieFighter.y > 430) {
+        tieFighter.y = 430;
+    }
+}
 
     function fireRocket(x, y, velocity) {
         this.x = x;
@@ -88,18 +104,31 @@ function playerBounds() {
 function updateGameArea() {
     myGameArea.clear();
     myGamePiece.speedX = 0;
-    myGamePiece.speedY = 0;    
+    myGamePiece.speedY = 0; 
+    tieFighter.speedX = 0;
+    tieFighter.speedY = 0;  
+
     if (myGameArea.key && myGameArea.key == 37) {myGamePiece.speedX = -4; }
     if (myGameArea.key && myGameArea.key == 39) {myGamePiece.speedX = 4; }
     if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -4; }
     if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 4; }
+      
+    if (myGameArea.key && myGameArea.key == 65) {tieFighter.speedX = -4; }
+    if (myGameArea.key && myGameArea.key == 68) {tieFighter.speedX = 4; }
+    if (myGameArea.key && myGameArea.key == 87) {tieFighter.speedY = -4; }
+    if (myGameArea.key && myGameArea.key == 83) {tieFighter.speedY = 4; }
     // if (myGameArea.key && myGameArea.key == 32) {this.fireRocket()}
 
-    
+   
 
     
     myGamePiece.newPos();    
     myGamePiece.update();
     playerBounds();
 
-}
+    tieFighter.newPos();    
+    tieFighter.update();
+    playerBounds();
+    tieBounds()
+
+    }
